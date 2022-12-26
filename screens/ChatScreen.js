42e44cadf4a1ed25b6f5fb70751dbbdb55f97f46
Abdulrahman Mohammed <abdulrahman48811@gmail.com>
2022-12-26 11:48:02 -0800

    updating chat screen format

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,45 +1,41 @@
-import React, { Component, useLayoutEffect, useState } from 'react';
-import { Text, View, StyleSheet} from 'react-native';
+import React, { Component, useLayoutEffect, useState } from "react";
+import { Text, View, StyleSheet } from "react-native";
 import { Input, Button } from "react-native-elements";
 import { db } from "../firebase";
 
- const ChatScreen = ({navigation}) => {
+const ChatScreen = ({ navigation }) => {
+  const [input, setInput] = useState("");
 
-  const [input, setInput] = useState("")
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: "Chat With Me!",
+    });
+  }, [navigation]);
 
-    useLayoutEffect(() => {
-      navigation.setOptions({
-        title: "Chat With Me!",
+  const createChat = async () => {
+    await db
+      .collection("chats")
+      .add({
+        chatName: input,
       })
-    }, [navigation])
-
-
-    const createChat = async () => {
-      await db.collection('chats').add({
-        chatName: input
-      }).then(()=> {
-        navigation
-      }).catch((error) => alert(error))
-    }
-
+      .then(() => {
+        navigation;
+      })
+      .catch((error) => alert(error));
+  };
 
   return (
     <View style={styles.container}>
-      <Input 
-      
-        placeholder= "Enter a chat name" 
+      <Input
+        placeholder="Enter your chat name"
         value={input}
         onChangeText={(text) => setInput(text)}
         onSubmitEditing={createChat}
       />
-      <Button
-                color= 'green'
-             
-               onPress={createChat} 
-               title="Create a new Chat" />
+      <Button color="black" onPress={createChat} title="Create a new Chat" />
     </View>
-  )
- }
+  );
+};
 
 export default ChatScreen;
 
@@ -48,5 +44,5 @@ const styles = StyleSheet.create({
     backgroundColor: "white",
     padding: 30,
     height: "100%",
-  }
+  },
 });
