2ac0f6f53286674c7ca71224e89c399b1fdf7a81
Abdulrahman Mohammed <abdulrahman48811@gmail.com>
2022-12-26 17:53:07 -0800

    added quran api, trying to show data properly

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,6 +2,9 @@ import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { auth } from "../firebase";
 import { useNavigation } from '@react-navigation/core';
+import { ActivityIndicator } from 'react-native';
+import { useEffect, useState } from 'react';
+
 
 const HomeScreen = () => {
     const navigation = useNavigation()
@@ -13,10 +16,40 @@ const HomeScreen = () => {
             })
             .catch(error => alert(error.message))
     }
+    let [isLoading, setIsLoading] = useState(true);
+    let [error, setError] = useState();
+    let [response, setResponse] = useState();
+
+    useEffect(() => {
+        fetch("https://api.quran.com/api/v4/verses/random?language=en&words=true")
+        .then(res => res.json())
+        .then((result) => {
+            setIsLoading(false);
+            setResponse(result);
+        },
+        (error) => {
+            setIsLoading(false);
+            setError(error);
+        }
+        )
+    }, [])
+
+    const getContent = () => {
+        if (isLoading) {
+
+            return <ActivityIndicator size="large" />;
+        }
+        if (error) {
+            return <Text>{error}</Text>
+        }
+        // console.log(response);
+        return <Text>Ayah Of The Day: {id}</Text>
+    }
 
 
     return (
         <View style={styles.container}>
+            {getContent()}
             <Text>Email: {auth.currentUser?.email}</Text>
             <TouchableOpacity
                 onPress={handleSignOut}
