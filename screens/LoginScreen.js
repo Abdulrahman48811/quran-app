c8dfad1965aae5720017461995bf14d91bfc3723
Abdulrahman Mohammed <abdulrahman48811@gmail.com>
2023-01-02 22:22:48 -0800

    adding logo to login

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,139 +1,161 @@
 import { useNavigation } from "@react-navigation/core";
 import React, { useEffect, useState } from "react";
 import {
-    StyleSheet,
-    Text,
-    TextInput,
-    View,
-    KeyboardAvoidingView,
-    TouchableOpacity,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  Image,
+  KeyboardAvoidingView,
+  TouchableOpacity,
 } from "react-native";
+import { useTailwind } from "tailwind-rn";
 import { auth } from "../firebase";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { TailwindProvider } from 'tailwindcss-react-native';
+import { Dimensions } from 'react-native';
+
 
 const LoginScreen = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+  const tw = useTailwind();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const navigation = useNavigation();
+  const navigation = useNavigation();
 
-    useEffect(() => {
-        const unsubscribe = auth.onAuthStateChanged((user) => {
-            if (user) {
-                navigation.replace("Tabs");
-            }
-        });
-        return unsubscribe;
-    }, []);
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (user) {
+        navigation.replace("Tabs");
+      }
+    });
+    return unsubscribe;
+  }, []);
 
-    const handleSignUp = () => {
-        auth
-            .createUserWithEmailAndPassword(email, password)
-            .then((userCredentials) => {
-                const user = userCredentials.user;
-            })
-            .catch((error) => alert(error.message));
-    };
+  const handleSignUp = () => {
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((userCredentials) => {
+        const user = userCredentials.user;
+      })
+      .catch((error) => alert(error.message));
+  };
 
-    const handleLogin = () => {
-        auth
-            .signInWithEmailAndPassword(email, password)
-            .then((userCredentials) => {
-                const user = userCredentials.user;
-            })
-            .catch((error) => alert(error.message));
-    };
+  const handleLogin = () => {
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .then((userCredentials) => {
+        const user = userCredentials.user;
+      })
+      .catch((error) => alert(error.message));
+  };
 
-    return (
-        <KeyboardAvoidingView style={styles.container} behavior="padding">
+  return (
+    <KeyboardAvoidingView style={styles.container} behavior="padding">
+       <TailwindProvider>
+       <SafeAreaView>
+      <View>
+        <Image style={styles.image} source={require("../assets/logo.png")} />
+      </View>
+      </SafeAreaView>
+      </TailwindProvider>
+      <View>
+        <View style={{}}></View>
+        <Text
+          style={{
+            color: "#da9100",
+            fontSize: 30,
+            fontFamily: "Cochin",
+            fontWeight: "bold",
+            marginBottom: 35,
+            padding: 15,
+          }}
+        >
+          Welcome To Quran Chat!
+        </Text>
+      </View>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Email"
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+          style={styles.input}
+        />
+        <TextInput
+          placeholder="Password"
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+          style={styles.input}
+          secureTextEntry
+        />
+      </View>
 
-            <View>
-                <View style={{}} ></View>
-                <Text style={{
-                    color: "#da9100",
-                    fontSize: 30,
-                    fontFamily: "Cochin",
-                    fontWeight: "bold",
-                    marginBottom: 35,
-                    padding: 15,
-                }}>Test Your Quran!</Text>
-            </View>
-            <View style={styles.inputContainer}>
-                <TextInput
-                    placeholder="Email"
-                    value={email}
-                    onChangeText={(text) => setEmail(text)}
-                    style={styles.input}
-                />
-                <TextInput
-                    placeholder="Password"
-                    value={password}
-                    onChangeText={(text) => setPassword(text)}
-                    style={styles.input}
-                    secureTextEntry
-                />
-            </View>
-
-            <View style={styles.buttonContainer}>
-                <TouchableOpacity onPress={handleLogin} style={styles.button}>
-                    <Text style={styles.buttonText}>Login</Text>
-                </TouchableOpacity>
-                <TouchableOpacity
-                    onPress={handleSignUp}
-                    style={[styles.button, styles.buttonOutline]}
-                >
-                    <Text style={styles.buttonOutlineText}>Register</Text>
-                </TouchableOpacity>
-            </View>
-        </KeyboardAvoidingView>
-    );
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity onPress={handleLogin} style={styles.button}>
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={handleSignUp}
+          style={[styles.button, styles.buttonOutline]}
+        >
+          <Text style={styles.buttonOutlineText}>Register</Text>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
+  );
 };
 
 export default LoginScreen;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-    },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  image: {
+    marginTop: -200,
+    marginBottom: 60,
+    width: 160, height: 160,
+  },
 
-    inputContainer: {
-        width: "80%",
-    },
-    input: {
-        backgroundColor: "white",
-        paddingHorizontal: 15,
-        paddingVertical: 10,
-        borderRadius: 10,
-        marginTop: 5,
-    },
-    buttonContainer: {
-        width: "60%",
-        justifyContent: "center",
-        alignItems: "center",
-        marginTop: 40,
-    },
-    button: {
-        backgroundColor: "#da9100",
-        width: "100%",
-        padding: 15,
-        borderRadius: 10,
-        alignItems: "center",
-    },
-    buttonOutline: {
-        backgroundColor: "white",
-        marginTop: 5,
-        borderColor: "#da9100",
-        borderWidth: 2,
-    },
-    buttonText: {
-        color: "white",
-        fontWeight: "700",
-        fontSize: 16,
-    },
-    buttonOutlineText: {
-        color: "#da9100",
-        fontWeight: "700",
-        fontSize: 16,
-    },
+  inputContainer: {
+    width: "80%",
+  },
+  input: {
+    backgroundColor: "white",
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderRadius: 10,
+    marginTop: 5,
+  },
+  buttonContainer: {
+    width: "60%",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 40,
+  },
+  button: {
+    backgroundColor: "#da9100",
+    width: "100%",
+    padding: 15,
+    borderRadius: 10,
+    alignItems: "center",
+  },
+  buttonOutline: {
+    backgroundColor: "white",
+    marginTop: 5,
+    borderColor: "#da9100",
+    borderWidth: 2,
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "700",
+    fontSize: 16,
+  },
+  buttonOutlineText: {
+    color: "#da9100",
+    fontWeight: "700",
+    fontSize: 16,
+  },
 });
